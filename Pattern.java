967c929b4483054596524f32d854cae301107b95
Polly <71594933+PollyDimieva@users.noreply.github.com>
2020-10-08 10:43:23 +0300

    Add files via upload
    how to reverse it?

---
+++ Pattern.java
@@ -0,0 +1,15 @@
+package edu;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int rows=6;  
+		for(int i=1;i<=rows;i++){  
+		for(int j=rows;j>=i;j--){  
+		        System.out.print(" *");  
+		}  
+		System.out.println();  
+		}  
+		}  
+		}  
