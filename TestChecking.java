d9ac96e5e36cbab1ee15341900381c162a68dc7c
Polly <71594933+PollyDimieva@users.noreply.github.com>
2020-10-15 17:35:12 +0300

    Update TestChecking.java

--- TestChecking.java
+++ TestChecking.java
@@ -7,36 +7,36 @@ public class TestChecking {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		char[] keyAnswers = new char[5];
+		char[] keyAnswers = new char[10];
 		{
 			keyAnswers[0] = 'D';
 			keyAnswers[1] = 'B';
 			keyAnswers[2] = 'D';
 			keyAnswers[3] = 'C';
 			keyAnswers[4] = 'C';
-			// keyAnswers[5]='D';
-			// keyAnswers[6]='A';
-			// keyAnswers[7]='E';
-			// keyAnswers[8]='A';
-			// keyAnswers[9]='D';
+		        keyAnswers[5]='D';
+		        keyAnswers[6]='A';
+			keyAnswers[7]='E';
+			keyAnswers[8]='A';
+			keyAnswers[9]='D';
 
-			char[][] answers = new char[3][5];
+			char[][] answers = new char[8][10];
 
 			int points = 0;
 
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < 8; i++) {
 				System.out.println("Input the answers to student number "+(i+1));
 				
-				for (int j = 0; j < 5; j++) {
+				for (int j = 0; j < 10; j++) {
 					
 					answers[i][j] = sc.nextLine().charAt(0);
 				}
 			}
 		
 
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < 8; i++) {
 				points = 0;
-				for (int j = 0; j < 5; j++) {
+				for (int j = 0; j < 10; j++) {
 					if (answers[i][j] == keyAnswers[j]) {
 						points++;
 					}
