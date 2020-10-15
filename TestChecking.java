15a9af5c473a362c4e9c2a0df6c19e7c97723132
Polly <71594933+PollyDimieva@users.noreply.github.com>
2020-10-15 17:30:15 +0300

    Add files via upload

--- TestChecking.java
+++ TestChecking.java
@@ -1,47 +1,44 @@
-package edu;
+package edu.smg;
 import java.util.Scanner;
 public class TestChecking {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 
-char [] keyAnswers = new char [10];
+	char [] keyAnswers = new char [5];{
+	keyAnswers[0]='D';
+	keyAnswers[1]='B';
+	keyAnswers[2]='D';
+	keyAnswers[3]='C';
+	keyAnswers[4]='C';
+//	keyAnswers[5]='D';
+//	keyAnswers[6]='A';
+//	keyAnswers[7]='E';
+//	keyAnswers[8]='A';
+//	keyAnswers[9]='D';
 
-keyAnswers[0]='D';
-keyAnswers[1]='B';
-keyAnswers[2]='D';
-keyAnswers[3]='C';
-keyAnswers[4]='C';
-keyAnswers[5]='D';
-keyAnswers[6]='A';
-keyAnswers[7]='E';
-keyAnswers[8]='A';
-keyAnswers[9]='D';
+	char [][] answers = new char [3][5];
 
+	int points = 0;
 
-char [][] answers = new char [2][5];
-
-int points = 0;
-
-for(int i=0;i<2;i++) {
-	for(int j=0;j<5;j++) {
-		answers [i][j]=sc.next().charAt(j);
+	for(int i=0;i<3;i++) {
+		for(int j=0;j<5;j++) {
+			answers [i][j]=sc.nextLine().charAt(0);
+		}
 	}
-} System.out.print(answers);
+	//} System.out.print(answers);
 
-//for(int i = 0; i<8;i++) {
-//	points = 0;
-//	for(int j=0;j<10;j++) {
-//		if(answers[i][j].equals(keyAnswers[j])) {
-//			points++;
-//		}
-//	}
-//	
-//	}
-//System.out.println(points);
+	for(int i = 0; i<3;i++) {
+		points = 0;
+		for(int j=0;j<5;j++) {
+			if(answers[i][j]==keyAnswers[j]) {
+				points++;
+			}
+		}
+		
+		}
+	System.out.println(points);
 
+
+		}
 
 	}
-
-}
