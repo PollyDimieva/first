c0dc3b2ddf48f472a57414dfe886ade7058cc1f6
Polly <71594933+PollyDimieva@users.noreply.github.com>
2020-11-23 14:04:23 +0200

    Add files via upload

--- myPointTest.java
+++ myPointTest.java
@@ -14,7 +14,7 @@ p2.setX(4.0);
 p2.setY(2.0);
 
 		System.out.println(p1.distance(0,0));
-		System.out.println(p1.distance(1, 2));
+		System.out.println(p1.distance( 1, 2));
 		System.out.println(p1.distance(p2));
 		System.out.println(myPoint.distance(p1, p2));
 		
