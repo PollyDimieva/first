7ba3793ccc31bc37b586e051f3a8932147744a7b
Polly <71594933+PollyDimieva@users.noreply.github.com>
2020-11-29 13:05:50 +0200

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,43 @@
+package edu;
+
+public class Circle {
+ public double radius;
+ 
+ 
+ public Circle(){
+	 radius = 1;
+ }
+	
+public Circle(double newRadius){
+	radius = newRadius;
+}
+	
+	public double getArea() {
+	
+		double a = Math.PI*radius*radius;
+		double area = (double)Math.round(a*100)/100;
+		return area;
+	}
+	
+	public double getPerimeter() {
+		double p = 2*Math.PI*radius;
+		double perimeter = (double)Math.round(p*100)/100;
+		return perimeter;
+	}
+	
+	public void setRadius(double newRadius) {
+		radius = newRadius;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
