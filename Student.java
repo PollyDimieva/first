4c6bbdcaccbe82673dcf4ec8def71471945f8a50
Polly <71594933+PollyDimieva@users.noreply.github.com>
2020-12-01 23:38:31 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+package edu;
+
+public class Student {
+    
+	public String name;
+	public double grades;
+	
+	public Student(){
+		
+	}
+	public Student(String name, double grades) {
+		this.name=name;
+		this.grades=grades;
+	}
+	
+}
