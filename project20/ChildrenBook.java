c4c3d50b9757a4a22f91330e53b39ed6742d65de
Polly <71594933+PollyDimieva@users.noreply.github.com>
2020-12-11 18:50:31 +0200

    Add files via upload

---
+++ project20/ChildrenBook.java
@@ -0,0 +1,26 @@
+package project20;
+
+public class ChildrenBook extends Book {
+
+	private int age;
+	
+	public ChildrenBook(double regularPrice, String publisher, int yearPublished, int age) {
+		super(regularPrice,publisher,yearPublished);
+		this.setAge(age);
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+    // Override this method
+    public double computeSalePrice(){
+        return super.getRegularPrice() - super.getRegularPrice()* 0.7;
+    }
+	
+	
+}
