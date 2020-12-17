fe03c6146b931326f9b47d40cd6a9dcd140e78eb
Polly <71594933+PollyDimieva@users.noreply.github.com>
2020-12-17 17:18:03 +0200

    Update myPointTest.java

--- myPointTest.java
+++ myPointTest.java
@@ -5,13 +5,12 @@ public class myPointTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 myPoint p1=new myPoint();
-myPoint p2 = new myPoint();
+myPoint p2 = new myPoint(4.0,2.0);
 
 p1.setX(8.0);
 p1.setY(6.0);
 
-p2.setX(4.0);
-p2.setY(2.0);
+
 
 		System.out.println(p1.distance(0,0));
 		System.out.println(p1.distance( 1, 2));
